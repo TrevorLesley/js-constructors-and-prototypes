81df1cff6401f9e527b39d9af2560b185b921be6
Trevor Lesley <tlesley96@gmail.com>
2021-05-12 14:30:47 -0400

    Built object constructors, having connection issues, pushing to restart hardware.

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,8 @@
 // \__,_/\__/_/_/_/\__/_/\___/____/
 
 // a simple "it" function for naming groups of expectations
-function it(description, contents){
-  console.log('\n\n"It '+ description + '"');
+function it(description, contents) {
+  console.log('\n\n"It ' + description + '"');
   contents();
 }
 
@@ -17,12 +17,12 @@ function it(description, contents){
 // Ex: expect(oz.color).toBe('red'); // should return true
 function expect(target) {
   return {
-    toBe: function(expectation) {
+    toBe: function (expectation) {
       if (target === expectation) {
-        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation );
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation);
         return true;
       } else {
-        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation );
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation);
         return false;
       }
     }
@@ -36,6 +36,23 @@ function expect(target) {
 // \___/\____/_/ /_/____/\__/_/   \__,_/\___/\__/\____/_/  /____/
 //
 // ONLY ADD CODE TO THIS SECTION
+
+function Human({ name, cool } = {}) {
+  this.name = name;
+  this.cool = cool;
+}
+
+function Dog({ name, color, hungry, owner, status } = {}) {
+  this.name = name;
+  this.color = color;
+  this.hungry = hungry;
+  this.owner = owner;
+  this.status = status;
+}
+
+Dog.prototype.status = function (status) {
+  this.status = status;
+}
 
 
 
@@ -82,33 +99,33 @@ let charlie = new Dog();
 // Don't edit this section. Instead make these tests pass by writing
 // constructors in the constructor section above ;D
 
-it("should make Oz happy when Mady pets him", function(){
+it("should make Oz happy when Mady pets him", function () {
   expect(oz.status).toBe('normal');
   mady.pet(oz);
   expect(oz.status).toBe('happy');
 });
 
-it("should make Oz red", function(){
+it("should make Oz red", function () {
   expect(oz.color).toBe('red');
 });
 
-it("should be make Moonshine hungry and Oz not hungry", function(){
+it("should be make Moonshine hungry and Oz not hungry", function () {
   expect(moonshine.hungry).toBe(true);
   expect(oz.hungry).toBe(false);
 });
 
-it("should make Moonshine no longer hungry when you feed her", function(){
+it("should make Moonshine no longer hungry when you feed her", function () {
   faith.feed(moonshine);
   expect(moonshine.hungry).toBe(false);
 });
 
-it("should not affect Charlie's or Moonshine's owner properties when setting Mady as Oz's owner ", function(){
+it("should not affect Charlie's or Moonshine's owner properties when setting Mady as Oz's owner ", function () {
   oz.owner = mady;
   expect(moonshine.owner?.name).toBe('Faith');
   expect(charlie.owner?.name).toBe(undefined);
 });
 
-it("should make Faith cool and Mady not cool", function(){
+it("should make Faith cool and Mady not cool", function () {
   expect(faith.cool).toBe(true);
   expect(mady.cool).toBe(false);
 });
